28308ee98055b7c15aeeae2d565029980e26e872
EscapedShadows <web@escapedshadows.com>
2024-10-21 21:26:28 +0200

    indev

---
+++ LGBTQuify.py
@@ -0,0 +1,4 @@
+import os
+
+if not os.path.isfile("index.json"):
+    print("Oopsies")
--- modules/updater.py
+++ modules/updater.py
@@ -1,3 +1,15 @@
 import requests
+from packaging.version import Version
 
-github_version = requests.get()
+def updater(local):
+    local_version = Version(local)
+    github_version = Version(requests.get("https://raw.githubusercontent.com/EscapedShadows/LGBTQuify/refs/heads/main/version.txt").text)
+
+    if github_version > local_version:
+        print("="*10)
+        print("Update available!")
+        print("="*10)
+        install = input("Would you like to install the update? (y/n): ")
+        install = True if install.lower() == "y" else False
+
+        return install
